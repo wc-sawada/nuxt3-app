5b0eb90345fa5c5540d8ecb2de95ed79fc5cd72f
澤田羽衣 <38057354+wc-sawada@users.noreply.github.com>
2022-09-27 17:36:12 +0900

    実験中

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <!-- Markup shared across all pages, ex: NavBar -->
+    <NuxtPage />
+  </div>
+</template>
+<!--
+<template>
+  <div>
     <NuxtWelcome />
   </div>
 </template>
+-->
